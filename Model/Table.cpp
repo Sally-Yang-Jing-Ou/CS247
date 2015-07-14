ce27e8a1af493a27e75f17d9002e84a3fcc8be87
Sally-Yang-Jing-Ou <sally_auyoung@hotmail.com>
2015-07-13 20:11:00 -0400

    check rep variant testing, commented out for now

--- Model/Table.cpp
+++ Model/Table.cpp
@@ -16,18 +16,33 @@ Table::~Table() {
     delete arrayOfSets_;
 };
 
+//#define checkRep returnArrayOfSets
+
 vector<vector<Card*>* >* Table::returnArrayOfSets() {
+	// assert(arrayOfSets_);
+	// for (int i = 0; i < arrayOfSets_->size(); ++i)
+	// 	for (int j = 0; j < arrayOfSets_->at(i)->size(); ++j) {
+	// 		const Card *c = arrayOfSets_->at(i)->at(j);
+	// 		assert(c);
+	// 		int r = (int)c->getRank(), s = (int)c->getSuit();
+	// 		assert(0 <= r && r < 13);
+	// 		assert(0 <= s && s < 4);
+	// 	}
 	return arrayOfSets_;
 }
 
 void Table::placeCard(Card *card) {
+    //checkRep();
     arrayOfSets_->at(card->getSuit())->push_back(card);
+    //checkRep();
 }
 
 void Table::clearTable() {
+    //checkRep();
     for (int i = 0; i < 4; i++) {
         arrayOfSets_->at(i)->clear();
     }
+    //checkRep();
 }
 
 bool lessThan(const Card *lhs, const Card *rhs)
@@ -36,6 +51,7 @@ bool lessThan(const Card *lhs, const Card *rhs)
 }
 
 void Table::printTable() {
+    //checkRep();
 	cout << "Cards on the table:" << endl;
 	cout << "Clubs:";
 	vector<vector<Card*>* >* arrayOfSets = returnArrayOfSets();
@@ -54,4 +70,5 @@ void Table::printTable() {
 			cout << "" << endl;
 		}
 	}
+    //checkRep();
 }
